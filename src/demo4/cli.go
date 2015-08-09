693cd6c1677673cd63d4132dfb5d17aa42867368
Spiegel <spiegel.im.spiegel@gmail.com>
2015-08-09 18:10:27 +0900

    add demo4.

---
+++ src/demo4/cli.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spiegel-im-spiegel/gitioapi"
+)
+
+// Exit codes are int values that represent an exit code for a particular error.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments.
+func (cli *CLI) Run(args []string) int {
+	var (
+		c string
+		url string
+	)
+
+	// Define option flag parse
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.SetOutput(cli.errStream)
+
+	flags.StringVar(&c, "c", "", "'code' parameter.")
+
+	flVersion := flags.Bool("version", false, "Print version information and quit.")
+
+	// Parse commandline flag
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	// Show version
+	if *flVersion {
+		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
+		return ExitCodeOK
+	}
+
+	// Parse argument
+	url = ""
+	flags.Parse(args[1:])
+	for flags.NArg() > 0 {
+		if len(url) == 0 {
+			url = flags.Args()[0]
+		} else {
+			fmt.Fprintln(cli.errStream, os.ErrInvalid, flags.Args()[0])
+			return ExitCodeError
+		}
+		flags.Parse(flags.Args()[1:])
+	}
+
+	// shortening URL
+	shortUrl, err := gitioapi.Encode(&gitioapi.Param{Url: url, Code: c})
+	if err != nil {
+		fmt.Fprintln(cli.errStream, err)
+		return ExitCodeError
+	}
+	fmt.Fprint(cli.outStream, shortUrl)
+
+	return ExitCodeOK
+}
---
+++ src/demo4/cli_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+type testCase struct { //Test case for Param
+	cmdline string
+	code int
+}
+
+var testCases []testCase //Test cases for Param
+
+
+func TestMain(m *testing.M) {
+	//Test cases for CLI
+	testCases = []testCase{
+		{cmdline: "./demo4 https://github.com/technoweenie", code: ExitCodeOK },
+		{cmdline: "./demo4 -c t https://github.com/technoweenie", code: ExitCodeOK },
+		{cmdline: "./demo4 -c t", code: ExitCodeError },
+		{cmdline: "./demo4", code: ExitCodeError },
+	}
+
+	//start test
+	code := m.Run()
+
+	//termination
+	os.Exit(code)
+}
+
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./demo4 -version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("demo4 version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_Arg(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	for _, test := range testCases {
+		args := strings.Split(test.cmdline, " ")
+		status := cli.Run(args)
+		if status != test.code {
+			t.Errorf("expected %d to eq %d", status, test.code)
+		}
+	}
+}
